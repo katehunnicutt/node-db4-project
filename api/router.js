e7fcfbaa1d01ec519a37616f62003f0dec637a9e
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-09-09 21:21:39 -0700

    finished

--- api/router.js
+++ api/router.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Recipe = require('./model')
 
+router.get('/:recipe_id', (req, res, next) => {
+  Recipe.getRecipeById(req.params.recipe_id)
+  .then(resource => {
+    res.status(200).json(resource)
+  })
+  .catch(next)
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+  res.status(500).json({
+    customMessage: 'it be broken',
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,18 +1,18 @@
 const express = require("express") //sets up express framework stores in constant
 const helmet = require("helmet")
-const router = require("./router")
+const recipesRouter = require("./router")
 
 const server = express() //initializing framework and storing in another constant
 
 server.use(helmet()) //secures http headers
 server.use(express.json()) //parse
-server.use('/api', router)
 
-server.use((err, req, res, next) => { //eslint-disable-line
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack
-  })
+server.use('/api/recipes', recipesRouter)
+
+
+server.use('*', (req, res) => {
+  res.json({ api: 'up'})
 })
+
 
 module.exports = server ; //expose
